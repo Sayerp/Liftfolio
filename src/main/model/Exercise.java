643db18055659dbec939138967610540374017e1
Preyas <preyas@students.cs.ubc.ca>
2025-10-09 01:52:52 -0700

    docs: add specification for Exercise class

---
+++ src/main/model/Exercise.java
@@ -0,0 +1,61 @@
+package model;
+
+// Represents an exercise.
+public class Exercise {
+
+    // REQUIRES: string length > 0
+    // EFFECTS: instantiates an exercise with given name, and an empty ArrayList of
+    // ExerciseSet
+    public Exercise(String name) {
+        // stub
+    }
+
+    // REQUIRES: reps > 0, weight > 0, and weight in pounds (lbs)
+    // MODIFIES: this
+    // EFFECTS: adds a new ExerciseSet with the given reps and weight to sets
+    public void addSet(int reps, int weight) {
+        // stub
+    }
+
+    // REQUIRES: sets.size() > 0, and 0 <= index <= sets.size()
+    // MODIFIES: this
+    // EFFECTS: removes ExerciseSet at given index position from sets
+    public void removeSet(int index) {
+        // stub
+    }
+
+    // REQUIRES: sets.size() > 0, 0 <= index <= sets.size(), reps > 0, weight > 0,
+    // and weight in pounds (lbs)
+    // MODIFIES: ExerciseSet
+    // EFFECTS: updates the reps and weight of ExerciseSet at given index position
+    // in sets
+    public void editSet(int index, int reps, int weight) {
+        // stub
+    }
+
+    // REQUIRES: sets.size() > 0
+    // EFFECTS: return the sum of volume from each ExerciseSet in sets
+    public int calculateTotalVolume() {
+        return 0; // stub
+    }
+
+    // REQUIRES: sets.size() > 0
+    // EFFECTS: return the largest estimated one rep max in sets
+    public int calculateOneRepMax() {
+        return 0; // stub
+    }
+
+    // REQUIRES: sets.size() > 0
+    // EFFECTS: return the weighted average of weights from each ExerciseSet in sets
+    public int calculateAverageWeight() {
+        return 0; // stub
+    }
+
+    public void setName(String name) {
+        // stub
+    }
+
+    public void getSets() {
+        // stub
+    }
+}
