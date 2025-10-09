5040c821ea2e0dfe5063360aeb8491370e3b5097
Preyas <preyas@students.cs.ubc.ca>
2025-10-09 14:45:56 -0700

    feat: Implement Exercise class

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -4,47 +4,67 @@ import java.util.ArrayList;
 
 // Represents an exercise.
 public class Exercise {
+    private String name;
+    private ArrayList<ExerciseSet> sets;
 
     // REQUIRES: string length > 0
     // EFFECTS: instantiates an exercise with given name, and an empty ArrayList of
-    // ExerciseSet
+    //          ExerciseSet
     public Exercise(String name) {
-        // stub
+        this.name = name;
+        sets = new ArrayList<>();
     }
 
     // REQUIRES: reps > 0, weight > 0, and weight in pounds (lbs)
     // MODIFIES: this
     // EFFECTS: adds a new ExerciseSet with the given reps and weight to sets
     public void addSet(int reps, double weight) {
-        // stub
+        ExerciseSet set = new ExerciseSet(reps, weight);
+        sets.add(set);
     }
 
     // REQUIRES: sets.size() > 0, and 0 <= index <= sets.size()
     // MODIFIES: this
     // EFFECTS: removes ExerciseSet at given index position from sets
     public void removeSet(int index) {
-        // stub
+        sets.remove(index);
     }
 
     // REQUIRES: sets.size() > 0, 0 <= index <= sets.size(), reps > 0, weight > 0,
     // and weight in pounds (lbs)
     // MODIFIES: ExerciseSet
     // EFFECTS: updates the reps and weight of ExerciseSet at given index position
-    // in sets
+    //          in sets
     public void editSet(int index, int reps, double weight) {
-        // stub
+        ExerciseSet set = sets.get(index);
+        set.setReps(reps);
+        set.setWeight(weight);
     }
 
     // REQUIRES: sets.size() > 0
     // EFFECTS: return the sum of volume from each ExerciseSet in sets
     public double calculateTotalVolume() {
-        return 0; // stub
+        double totalVolume = 0;
+
+        for (ExerciseSet set : sets) {
+            totalVolume += set.calculateVolume();
+        }
+
+        return totalVolume;
     }
 
     // REQUIRES: sets.size() > 0
     // EFFECTS: return the largest estimated one rep max in sets
     public double calculateOneRepMax() {
-        return 0; // stub
+        double oneRepMax = 0;
+
+        for (ExerciseSet set : sets) {
+            if (set.estimateOneRepMax() > oneRepMax) {
+                oneRepMax = set.estimateOneRepMax();
+            }
+        }
+
+        return oneRepMax;
     }
 
     // REQUIRES: sets.size() > 0
@@ -52,18 +72,26 @@ public class Exercise {
     //          rounded to one decimal
     //          totalVolume / totalReps
     public double calculateAverageWeight() {
-        return 0; // stub
+        double totalVolume = 0;
+        int totalReps = 0;
+
+        for (ExerciseSet set : sets) {
+            totalVolume += set.calculateVolume();
+            totalReps += set.getReps();
+        }
+
+        return Math.round(totalVolume / totalReps * 10.0) / 10.0;
     }
 
     public void setName(String name) {
-        // stub
+        this.name = name;
     }
 
     public String getName() {
-        return ""; // stub
+        return this.name;
     }
 
     public ArrayList<ExerciseSet> getSets() {
-        return null; // stub
+        return this.sets;
     }
 }
