f44fb8f0c268b761693dcff4e1443b63c21daf22
Preyas <preyas@students.cs.ubc.ca>
2025-10-08 22:57:33 -0700

    feat: Implement ExerciseSet class

--- src/main/model/ExerciseSet.java
+++ src/main/model/ExerciseSet.java
@@ -2,36 +2,41 @@ package model;
 
 // Represents an exercise set.
 public class ExerciseSet {
+    int reps;
+    double weight;
 
     // REQUIRES: reps > 0, weight > 0, and weight in pounds (lbs)
     // EFFECTS: constructs an ExerciseSet object with the given reps and weight
     public ExerciseSet(int reps, double weight) {
-        // stub
+        this.reps = reps;
+        this.weight = weight;
     }
 
-    // EFFECTS: return volume of the exercise set with one decimal place
+    // EFFECTS: return volume of the exercise set rounded to one decimal
     public double calculateVolume() {
-        return 0; // stub
+        double volume = reps * weight;
+        return Math.round(volume * 10.0) / 10.0;
     }
 
-    // EFFECTS: return estimated one rep max with one decimal place
+    // EFFECTS: return estimated one rep max rounded to one decimal
     public double estimateOneRepMax() {
-        return 0; //stub
+        double orm = weight * (1 + reps/30.0);
+        return Math.round(orm * 10.0) / 10.0;
     }
 
     public int getReps() {
-        return 0; // stub
+        return this.reps;
     }
 
     public double getWeight() {
-        return 0; // stub
+        return this.weight;
     }
 
     public void setReps(int reps) {
-        // stub
+        this.reps = reps;
     }
 
     public void setWeight(int weight) {
-        // stub
+        this.weight = weight;
     }
 }
