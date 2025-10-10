25252ea77f366053da59ef3b9795bd36d6ec483e
Preyas <preyas@students.cs.ubc.ca>
2025-10-10 08:43:48 -0700

    docs: add specification for WorkoutTrackerApp UI class

---
+++ src/main/ui/WorkoutTrackerApp.java
@@ -0,0 +1,155 @@
+package ui;
+
+// Workout tracker application that allows the user to log and view workouts
+// Structure of this class was made by referencing FlashcardReviewer from lab 4
+public class WorkoutTrackerApp {
+
+    // EFFECTS: instantiates WorkoutTrackerApp console UI application
+    public WorkoutTrackerApp() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes the application with starting values
+    public void init() {
+        // stub
+    }
+
+    // EFFECTS: displays and processes inputs for the main menu
+    public void handleMainMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays options that can be selected from main menu
+    public void displayMainMenu() {
+        // stub
+    }
+
+    // EFFECTS: processes user input in the main menu
+    public void processMainMenuCommands(String input) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new workout to workout history
+    public void addNewWorkout() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prints a closing message and marks the program as not running
+    public void quitApplication() {
+        // stub
+    }
+
+    // EFFECTS: displays and processes inputs for the view workout history menu
+    public void handleWorkoutHistoryMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays all workuts and options that can be selected from the view
+    //          workout history menu
+    public void displayWorkoutHistoryMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays all workouts one at a time
+    public void displayWorkoutHistory() {
+        // stub
+    }
+
+    // EFFECTS: processes user input in the view workout history menu
+    public void processWorkoutHistoryMenuCommands(String input) {
+        // stub
+    }
+
+    // EFFECTS: displays and processes inputs for the view workout menu
+    public void handleWorkoutMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays exercises in workout and options that can be selected from
+    //          the view workout menu
+    public void displayWorkoutMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays all exercises in workout one at a time
+    public void displayWorkout() {
+        // stub
+    }
+
+    // EFFECTS: processes user input in the view workout menu
+    public void processWorkoutMenuCommands(String input) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new exercise to workout
+    public void addExercise() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: renames the workout
+    public void renameWorkout() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes workout from workout history
+    public void removeWorkout() {
+        // stub
+    }
+
+    // EFFECTS: displays and processes inputs for the exercise menu
+    public void handleExerciseMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays exercise sets and options that can be selected from the
+    //          exercise menu
+    public void displayExerciseMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays all sets of an exercise one at a time
+    public void displayExercise() {
+        // stub
+    }
+
+    // EFFECTS: processes user input in the exercise menu
+    public void processExerciseMenuCommands(String input) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new set to exercise
+    public void addSet() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: change the reps and weight of a set
+    public void editSet() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: renames the exercise
+    public void renameExercise() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes set from exercise
+    public void removeSet() {
+        // stub
+    }
+
+    // This method is from the flashcard reviewer lab
+    // EFFECTS: prints a line of dashes to act as a divider
+    private void printDivider() {
+        System.out.println("----------------------------------------");
+    }
+}
