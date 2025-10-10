e94cfed0988477ba77e105e00f350d775c3fd058
Preyas <preyas@students.cs.ubc.ca>
2025-10-10 10:01:11 -0700

    feat: implement setup and addNewWorkout to WorkoutTrackerApp UI

--- src/main/ui/WorkoutTrackerApp.java
+++ src/main/ui/WorkoutTrackerApp.java
@@ -1,39 +1,118 @@
 package ui;
 
+import model.Exercise;
+import model.Workout;
+import model.WorkoutHistory;
+
+import java.util.Scanner;
+
 // Workout tracker application that allows the user to log and view workouts
 // Structure of this class was made by referencing FlashcardReviewer from lab 4
 public class WorkoutTrackerApp {
+    private WorkoutHistory workouts;
+
+    private Scanner scanner;
+    private Boolean isApplicationRunning;
 
     // EFFECTS: instantiates WorkoutTrackerApp console UI application
     public WorkoutTrackerApp() {
-        // stub
+        init();
+
+        printDivider();
+        System.out.println("Welcome to Liftfolio!");
+        printDivider();
+
+        while (this.isApplicationRunning) {
+            handleMainMenu();
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: initializes the application with starting values
     public void init() {
-        // stub
+        this.workouts = new WorkoutHistory();
+        this.scanner = new Scanner(System.in);
+        this.isApplicationRunning = true;
     }
 
     // EFFECTS: displays and processes inputs for the main menu
     public void handleMainMenu() {
-        // stub
+        displayMainMenu();
+        String input = this.scanner.nextLine();
+        processMainMenuCommands(input);
     }
 
     // EFFECTS: displays options that can be selected from main menu
     public void displayMainMenu() {
-        // stub
+        System.out.println("Please choose an option:\n");
+        System.out.println("1: Add Workout");
+        System.out.println("2: View/Edit Workouts");
+        System.out.println("3: Close Application");
+        printDivider();
     }
 
     // EFFECTS: processes user input in the main menu
     public void processMainMenuCommands(String input) {
-        // stub
+        printDivider();
+        switch (input) {
+            case "1":
+                addNewWorkout();
+                break;
+            case "2":
+                handleWorkoutHistoryMenu();
+                break;
+            case "3":
+                quitApplication();
+                break;
+            default:
+                System.out.println("Invalid input, please try again.");
+        }
+        printDivider();
     }
 
     // MODIFIES: this
     // EFFECTS: adds a new workout to workout history
     public void addNewWorkout() {
-        // stub
+        Boolean continueAdding = true;
+        
+        System.out.println("Please enter a workout name:");
+        String workoutName = this.scanner.nextLine();
+        Workout workout = new Workout(workoutName);
+
+        while (continueAdding) {
+            System.out.println("\nPlease enter an exercise name:");
+            String exerciseName = this.scanner.nextLine();
+
+            Exercise exercise = new Exercise(exerciseName);
+
+            System.out.println("\nPlease enter the number of sets:");
+            int sets = Integer.valueOf(this.scanner.nextLine());
+
+            for (int i=1; i <= sets; i++) {
+                System.out.println("\n Set-" + i + " Please enter reps:");
+                int reps = Integer.valueOf(this.scanner.nextLine());
+
+                System.out.println("\n Set-" + i + " Please enter weight:");
+                int weight = Integer.valueOf(this.scanner.nextLine());
+
+                exercise.addSet(reps, weight);
+            }
+            
+            workout.addExercise(exercise);
+            System.out.println("Added " + sets + " sets of " + exerciseName + " to " + workoutName + " workout!");
+
+            System.out.println("Would you like to add another exercise to the workout? y/n:");
+            String answer = this.scanner.nextLine();
+
+            while (!answer.equals("y") && !(answer.equals("n"))) {
+                System.out.println("Invalid input. Please enter 'y' or 'n'");
+                answer = this.scanner.nextLine();
+            }
+
+            if (answer.equals("n")) {
+                continueAdding = false;
+            }
+        }
     }
 
     // MODIFIES: this
