44f054bdc5de3da8cb3f665be04f8c749e0a0162
Preyas <preyas@students.cs.ubc.ca>
2025-10-10 15:52:24 -0700

    fix: typos in specification of Workout class

--- src/main/model/Workout.java
+++ src/main/model/Workout.java
@@ -8,39 +8,38 @@ public class Workout {
     private ArrayList<Exercise> exercises;
 
     // REQUIRES: string length > 0
-    // EFFECTS: instantiates a workout with given name, and and empty ArrayList of
-    // Exercise
+    // EFFECTS: instantiates a workout with given name, and no exercises
     public Workout(String name) {
         this.name = name;
         exercises = new ArrayList<>();
     }
 
     // MODIFIES: this
-    // EFFECTS: adds a new Exercise to exercises
+    // EFFECTS: adds an exercise to exercises
     public void addExercise(Exercise exercise) {
         exercises.add(exercise);
     }
 
-    // REQUIRES: exercises.size() > 0, and 0 <= index <= exercises.size()
+    // REQUIRES: exercises.size() > 0, and 0 <= index < exercises.size()
     // MODIFIES: this
-    // EFFECTS: removes Exercise at given index position from exercises
+    // EFFECTS: removes exercise at given index position from exercises
     public void removeExercise(int index) {
         exercises.remove(index);
     }
 
-    // REQUIRES: exercises.size() > 0, 0 <= exerciseIndex <= exercises.size(),
-    //           sets.size() of selected exercise > 0, 0 <= setIndex <= sets.size(), reps > 0,
+    // REQUIRES: exercises.size() > 0, 0 <= exerciseIndex < exercises.size(),
+    //           sets.size() of selected exercise > 0, 0 <= setIndex < sets.size(), reps > 0,
     //           weight > 0, and weight in pounds (lbs)
     // MODIFIES: ExerciseSet
-    // EFFECTS: updates reps and weight of ExerciseSet at setIndex for Exercise at
+    // EFFECTS: updates reps and weight of a set at setIndex for exercise at
     //          exerciseIndex in exercises
     public void editExercise(int exerciseIndex, int setIndex, int reps, double weight) {
         exercises.get(exerciseIndex).editSet(setIndex, reps, weight);
     }
 
-    // REQUIRES: exercises.size() > 0, 0 <= exerciseIndex <= exercises.size()
+    // REQUIRES: exercises.size() > 0, 0 <= exerciseIndex < exercises.size()
     // MODIFIES: Exercise
-    // EFFECTS: updates name of Exercise at exerciseIndex in exercises
+    // EFFECTS: updates name of exercise at exerciseIndex in exercises
     public void editExercise(int exerciseIndex, String name) {
         exercises.get(exerciseIndex).setName(name);
     }
