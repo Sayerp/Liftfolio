ec0d3ebebc24452b404c3843bb395c4a1a27a2f6
Preyas <preyas@students.cs.ubc.ca>
2025-10-24 00:45:40 -0700

    feat: implement JsonWriter class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,40 +1,46 @@
 package persistence;
 
 import model.WorkoutHistory;
+import org.json.JSONObject;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+
 
 // Represents a writer that writes JSON representation of workout history to file
 // Structure of this class and methods were made by referencing JsonSerializationDemo
 public class JsonWriter {
+    private static int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destionation file
     public JsonWriter(String destination) {
-        // stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     //          be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of workout history to file
-    public void write(WorkoutHistory workoutHistory) {
-        // stub
+    public void write(WorkoutHistory workouts) {
+        JSONObject workoutsJson = workouts.toJson();
+        saveToFile(workoutsJson.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
