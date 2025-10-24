4ae532950fc8d06da4c0c12ee4117c336c6e8633
Preyas <preyas@students.cs.ubc.ca>
2025-10-24 02:03:47 -0700

    style: correct indenting in JsonTest class

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -25,27 +25,27 @@ public class JsonTest {
     }
 
     protected void checkGeneralWorkoutHistory(WorkoutHistory wh) {
-            List<Workout> workouts = wh.getWorkouts();
-            assertEquals(2, workouts.size());
+        List<Workout> workouts = wh.getWorkouts();
+        assertEquals(2, workouts.size());
 
-            Workout legDay = workouts.get(0);
-            assertEquals("Leg Day", legDay.getName());
+        Workout legDay = workouts.get(0);
+        assertEquals("Leg Day", legDay.getName());
 
-            Exercise squats = legDay.getExercises().get(0);
-            checkExercise("Squats", 2, squats);
-            checkExerciseSet(6, 225, squats.getSets().get(0));
-            checkExerciseSet(3, 185, squats.getSets().get(1));
+        Exercise squats = legDay.getExercises().get(0);
+        checkExercise("Squats", 2, squats);
+        checkExerciseSet(6, 225, squats.getSets().get(0));
+        checkExerciseSet(3, 185, squats.getSets().get(1));
 
-            Workout chestDay = workouts.get(1);
-            assertEquals("Chest Day", chestDay.getName());
+        Workout chestDay = workouts.get(1);
+        assertEquals("Chest Day", chestDay.getName());
 
-            Exercise bench = chestDay.getExercises().get(0);
-            checkExercise("Bench Press", 1, bench);
-            checkExerciseSet(5, 315, bench.getSets().get(0));
+        Exercise bench = chestDay.getExercises().get(0);
+        checkExercise("Bench Press", 1, bench);
+        checkExerciseSet(5, 315, bench.getSets().get(0));
 
-            Exercise flys = chestDay.getExercises().get(1);
-            checkExercise("Cable Flys", 2, flys);
-            checkExerciseSet(8, 60, flys.getSets().get(0));
-            checkExerciseSet(5, 50, flys.getSets().get(1));
+        Exercise flys = chestDay.getExercises().get(1);
+        checkExercise("Cable Flys", 2, flys);
+        checkExerciseSet(8, 60, flys.getSets().get(0));
+        checkExerciseSet(5, 50, flys.getSets().get(1));
     }
 }
