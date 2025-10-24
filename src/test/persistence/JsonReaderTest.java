dbf7fcec44a101aa31ea638feed82fafc9f363d7
Preyas <preyas@students.cs.ubc.ca>
2025-10-23 23:11:26 -0700

    refactor: replace testReaderGeneralWorkoutHistory assertions with checkGeneralWorkoutHistory method

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -42,28 +42,7 @@ public class JsonReaderTest extends JsonTest {
         JsonReader reader = new JsonReader("data/testReaderGeneralWorkoutHistory.json");
         try {
             WorkoutHistory wh = reader.read();
-            List<Workout> workouts = wh.getWorkouts();
-            assertEquals(2, workouts.size());
-
-            Workout legDay = workouts.get(0);
-            assertEquals("Leg Day", legDay.getName());
-
-            Exercise squats = legDay.getExercises().get(0);
-            checkExercise("Squats", 2, squats);
-            checkExerciseSet(6, 225, squats.getSets().get(0));
-            checkExerciseSet(3, 185, squats.getSets().get(1));
-
-            Workout chestDay = workouts.get(1);
-            assertEquals("Chest Day", chestDay.getName());
-
-            Exercise bench = chestDay.getExercises().get(0);
-            checkExercise("Bench Press", 1, bench);
-            checkExerciseSet(5, 315, bench.getSets().get(0));
-
-            Exercise flys = chestDay.getExercises().get(1);
-            checkExercise("Cable Flys", 2, flys);
-            checkExerciseSet(8, 60, flys.getSets().get(0));
-            checkExerciseSet(5, 50, flys.getSets().get(1));
+            checkGeneralWorkoutHistory(wh);
         } catch (Exception e) {
             fail("Couldn't read from file");
         }
